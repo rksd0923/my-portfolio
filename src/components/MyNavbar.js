b74d3371b8b364c5e39110ce80a46af10d141120
Ruben Rivera <rerivera76@gmail.com>
2021-01-13 01:48:16 -0500

    first commit

---
+++ src/components/MyNavbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import './MyNavbar.css'
+
+const MyNavbar = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Ruben Rivera</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <Nav.Link href="#pricing">Skills</Nav.Link>
+                        <Nav.Link href="#pricing">Projects</Nav.Link>
+                        <Nav.Link href="#pricing">Contact</Nav.Link>
+
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export default MyNavbar
